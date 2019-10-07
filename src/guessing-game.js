e91cc7eedadfb847500e614b58feeabce26d0af8
iskanderror <iskanderror@gmail.com>
2019-10-07 12:56:48 +0500

    feat: binary search

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.min = 0;
+      this.max = 0;
+      this.guessResult = 0;
+    }
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
+      this.guessResult = Math.round((this.max-this.min)/2)+this.min;
+      return this.guessResult;
 
     }
 
     lower() {
-
+      this.max = this.guessResult;
     }
 
     greater() {
-
+      this.min = this.guessResult;
     }
 }
 
